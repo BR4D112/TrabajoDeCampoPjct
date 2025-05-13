e9c28b849a231ccd81fa7e2617a2bd46d05d2e43
Nicolas Gutierrez <yefry-nicolas123@hotmail.com>
2025-05-13 10:38:33 -0500

    fix(deploy) endpoints config changed

--- src/API/Endpoints.js
+++ src/API/Endpoints.js
@@ -1,20 +1,24 @@
-import { envConfig } from "../config/env.config";
+import { envConfig as API_URL } from "../config/env.config";
 
-export const LOGIN_URL = `${envConfig.apiUrl}/auth/login`;
-export const RECOVER_URL = `${envConfig.apiUrl}/auth/recover`;
-export const REGISTER_URL = `${envConfig.apiUrl}/auth/register`;
-export const LOGOUT_URL = `${envConfig.apiUrl}/auth/logout`;
-export const USER_URL = `${envConfig.apiUrl}/user`;
-export const PASSWORD_RESET = `${envConfig.apiUrl}/user`;
-export const CREATE_DOCENTE = `${envConfig.apiUrl}/teachers/create`;
-export const SEARCH_DOCENTE = `${envConfig.apiUrl}/teachers/search`;
-export const UPDATE_DOCENTE = `${envConfig.apiUrl}/teachers/`;
-export const SEARCH_ACADEMIC_PERIOD = `${envConfig.apiUrl}/academic_periods`;
-export const CREATE_ACADEMIC_PERIOD = `${envConfig.apiUrl}/academic-periods/register`;
-export const UPDATE_ACADEMIC_PERIOD = `${envConfig.apiUrl}/academic-periods/`;
-export const CREATE_SUBJECT = `${envConfig.apiUrl}/subjects/create`;
-export const SEARCH_SUBJECT = `${envConfig.apiUrl}/subjects/search`;
-export const UPDATE_SUBJECT = `${envConfig.apiUrl}/subjects/`;
-export const CREATE_GROUP = `${envConfig.apiUrl}/groups/create`;
-export const SEARCH_GROUP = `${envConfig.apiUrl}/groups/search`;
-export const UPDATE_GROUP = `${envConfig.apiUrl}/groups/`;
+
+const API_URL1 = "https://sage-backend-mxlb.onrender.com";
+
+export const LOGIN_URL = `${API_URL}/auth/login`;
+export const RECOVER_URL = `${API_URL1}/auth/recover`;
+export const REGISTER_URL = `${API_URL1}/auth/register`;
+export const LOGOUT_URL = `${API_URL1}/auth/logout`;
+export const USER_URL = `${API_URL1}/user`;
+export const PASSWORD_RESET = `${API_URL1}/user`;
+export const CREATE_DOCENTE = `${API_URL1}/teachers/create`;
+export const SEARCH_DOCENTE = `${API_URL1}/teachers/search`;
+export const UPDATE_DOCENTE = `${API_URL1}/teachers/`;
+export const SEARCH_ACADEMIC_PERIOD = `${API_URL1}/academic_periods`;
+export const CREATE_ACADEMIC_PERIOD = `${API_URL1}/academic-periods/register`;
+export const UPDATE_ACADEMIC_PERIOD = `${API_URL1}/academic-periods/`;
+export const CREATE_SUBJECT = `${API_URL1}/subjects/create`;
+export const SEARCH_SUBJECT = `${API_URL1}/subjects/search`;
+export const UPDATE_SUBJECT = `${API_URL1}/subjects/`;
+export const CREATE_GROUP = `${API_URL1}/groups/create`;
+export const SEARCH_GROUP = `${API_URL1}/groups/search`;
+export const UPDATE_GROUP = `${API_URL1}/groups/`;
+
